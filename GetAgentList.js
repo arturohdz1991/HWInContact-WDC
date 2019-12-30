a527fb7d92a4bdca175301577b933ad44f45b29d
arturohdz1991 <57507668+arturohdz1991@users.noreply.github.com>
2019-12-30 13:58:29 -0600

    Corrected Array

--- GetAgentList.js
+++ GetAgentList.js
@@ -17,7 +17,7 @@
     //Access data for HRCC
     accessData[6] = {'cluster':"HRCC",'email':"API_READONLY@HRCC.com",'password':"aP1_2020",'applicationID':"Admin@HRCC.com:4599199"}
     //Access data for DSES
-    accessData[6] = {'cluster':"DSES",'email':"Arturo.Hernandez2@DSES.com",'password':"Nov12345!",'applicationID':"Admin@DSES.com:4599200"}
+    accessData[7] = {'cluster':"DSES",'email':"Arturo.Hernandez2@DSES.com",'password':"Nov12345!",'applicationID':"Admin@DSES.com:4599200"}
     // Create the connector object
     var myConnector = tableau.makeConnector();
     // Define the schema
