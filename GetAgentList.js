03639f743a7666663cad648457b8b9cb96d818bf
arturohdz1991 <57507668+arturohdz1991@users.noreply.github.com>
2020-01-02 16:48:48 -0600

    Update GetAgentList.js

--- GetAgentList.js
+++ GetAgentList.js
@@ -18,38 +18,38 @@
     accessData[6] = {'cluster':"HRCC",'email':"API_READONLY@HRCC.com",'password':"aP1_2020",'applicationID':"Admin@HRCC.com:4599199"}
     //Access data for DSES
     accessData[7] = {'cluster':"DSES",'email':"Arturo.Hernandez2@DSES.com",'password':"Nov12345!",'applicationID':"Admin@DSES.com:4599200"}
-    // Create the connector object
+	//Variable to store how many Requests are left to do
+	ajaxCallsRemaining = Object.keys(accessData).length
+	console.log(ajaxCallsRemaining+" Requests")
+	// Create the connector object
     var myConnector = tableau.makeConnector();
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [
-            {id: "Cluster",dataType: tableau.dataTypeEnum.string},
-            {id: "agentId",alias:"User ID",dataType: tableau.dataTypeEnum.int},
-            {id: "firstName",dataType: tableau.dataTypeEnum.string},
-            {id: "lastName",dataType: tableau.dataTypeEnum.string},
-            {id: "emailAddress",alias:"NIC Email",dataType: tableau.dataTypeEnum.string},
-            {id: "isActive",dataType: tableau.dataTypeEnum.bool},
-            {id: "teamId",dataType: tableau.dataTypeEnum.int},
-			{id: "teamName",dataType: tableau.dataTypeEnum.string},
-			{id: "location",alias:"NIC Site",dataType: tableau.dataTypeEnum.string},
-            {id: "internalId",alias:"NIC EID",dataType: tableau.dataTypeEnum.string},
-            {id: "profileId",dataType: tableau.dataTypeEnum.int},
-            {id: "profileName",dataType: tableau.dataTypeEnum.string},
-            {id: "createDate",alias:"User Create Date",dataType: tableau.dataTypeEnum.datetime},
-            {id: "inactiveDate",alias:"User Inactive Date",dataType: tableau.dataTypeEnum.datetime},
-            {id: "isBillable",dataType: tableau.dataTypeEnum.bool}
+            {id: "Agent_No",dataType: tableau.dataTypeEnum.int},
+            {id: "Agent_Name",dataType: tableau.dataTypeEnum.string},
+            {id: "Email",dataType: tableau.dataTypeEnum.string},
+            {id: "Location",dataType: tableau.dataTypeEnum.string},
+            {id: "Team",dataType: tableau.dataTypeEnum.string},
+            {id: "Login_DateTime",dataType: tableau.dataTypeEnum.datetime},
+            {id: "Station_Id",dataType: tableau.dataTypeEnum.int},
+            {id: "Station_Phone_Num",dataType: tableau.dataTypeEnum.string},
+            {id: "Station_Caller_ID",dataType: tableau.dataTypeEnum.int},
+            {id: "ClientType",dataType: tableau.dataTypeEnum.string},
+            {id: "Agent_Session_ID",dataType: tableau.dataTypeEnum.int},
+            {id: "LoginStation_DateTime",dataType: tableau.dataTypeEnum.datetime}
         ];
 
         var tableSchema = {
-            id: "agentList",
-            alias: "User Lists",
+            id: "adminList",
+            alias: "Admin Lists",
             columns: cols
         };
 
         schemaCallback([tableSchema]);
     };
     //Function to request Access token
-    function getToken(accessVariable,callback){
+    function getToken(accessVariable,callback,table,doneCallBack){
         requestBody = {
             "grant_type" : "password",
             "username" : accessVariable.email,
@@ -67,49 +67,70 @@
             'success': function(result,status,statusCode){
                 //document.getElementById("result").innerHTML += "<br>1:" + JSON.stringify(result);
                 //accessToken = result
-                callback(accessVariable.cluster,result);
+                console.log(accessVariable.cluster + " Token:" + status);
+                callback(accessVariable.cluster,result,table,doneCallBack);
             },
             'error': function(XMLHttpRequest, textStatus, errorThrown){
                 //document.getElementById("result").innerHTML += "<br>1:" + JSON.stringify(errorThrown);
-                callback(null);
+                console.log(accessVariable.cluster + " Token:" + textStatus);
             }
         });
     }
     //Function to request data
-    function dataRequest(cluster, accessToken){
+    function dataRequest(cluster, accessToken,table,doneCallBack){
+        if (cluster == "HON"|cluster == "PMT"){
+            reportId = 1073742382;
+        } else if(cluster == "HBT"|cluster == "SPS"){
+            reportId = 1074092264;
+        } else if(cluster == "AERO"|cluster == "SPSEM"){
+            reportId = 1073741832;
+        } else if(cluster == "HRCC"|cluster == "DSES"){
+            reportId = 1073741825;
+        }
+        
+        startDate = new Date().toISOString()
+        endDate = new Date().toISOString()
+        includeHeaders = false
         requestBody = {
-            'updatedSince': '',
-            'isActive': '',
-            'searchString': '',
-            'fields': 'agentId,firstName,lastName,emailAddress,isActive,teamId,teamName,isBillable,location,profileId,profileName,createDate,inactiveDate,internalId',
-            'skip': '',
-            'top': '',
-            'orderBy': ''
+            
         }
         $.ajax({
-            'url':accessToken.resource_server_base_uri + "services/v16.0/agents",
-            'type':'GET',
+            'url':
+            accessToken.resource_server_base_uri + "services/v16.0/report-jobs/datadownload/" + reportId +
+            '?startDate=' + startDate + '&endDate=' + endDate + '&includeHeaders=' + includeHeaders,
+            'type':'POST',
             'headers':{
                 'Authorization':'bearer '+ accessToken.access_token,
                 'Content-Type':'application/x-www-form-urlencoded'
             },
             'data':requestBody,
             'success': function (result,status,statusCode){
-                //document.getElementById("result").innerHTML = Array.isArray(result.agents)
-                agentList = result.agents
-                for (record in agentList){
+                console.log(cluster + " Result:" + status);
+                csvResult = atob(result.file)
+                //csvResult.replace(/(\r\n|\n|\r)/gm,";")
+                csvRows=csvResult.split(/\n/)
+                //for (csvRow=0;csvRow<1;csvRow++){
+                for (csvRow=0;csvRow<csvRows.length;csvRow++){
+                    //console.log(csvRows[csvRow])
                     rowData = []
-                    rowData.push(cluster)
-                    agentDetails = agentList[record]
-                    for (dataPoint in agentDetails){
+                    agentDetails = csvRows[csvRow].split(',')
+                    for (dataPoint=0;dataPoint<agentDetails.length;dataPoint++){
+                        //console.log(agentDetails[dataPoint])
                         rowData.push(agentDetails[dataPoint])
                     }
-                    tableData.push(rowData)
-                }
-                //document.getElementById("result").innerHTML += "<br>2:" + tableData
+					tableData.push(rowData)
+				}
+				console.log(cluster+" Query Success")
+				--ajaxCallsRemaining
+				console.log(ajaxCallsRemaining+" Call Remain")
+				if (ajaxCallsRemaining==0) {
+					console.log("Execute Callback")
+					table.appendRows(tableData)
+					doneCallBack();
+				}
             },
             'error': function(XMLHttpRequest, textStatus, errorThrown){
-                document.getElementById("result").innerHTML = "<br>2:" +JSON.stringify(textStatus);
+                console.log(cluster + " Result:" + textStatus);
             }
         });
     }
@@ -117,13 +138,8 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         for (acObj in accessData){
-                getToken(accessData[acObj],dataRequest);
+                getToken(accessData[acObj],dataRequest,table, doneCallback);
             }
-        //alert(tableData)
-        setTimeout(function(){
-            table.appendRows(tableData)
-            doneCallback();
-        }, 10000);
     };
 
     tableau.registerConnector(myConnector);
@@ -131,7 +147,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "User List"; // This will be the data source name in Tableau
+            tableau.connectionName = "Admin Report"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
