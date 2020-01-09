daafc2603a6a72f877e070aa896fdd3edd9581ae
arturohdz1991 <57507668+arturohdz1991@users.noreply.github.com>
2020-01-09 10:27:41 -0600

    Update GetSiteDetails.js

--- GetSiteDetails.js
+++ GetSiteDetails.js
@@ -43,7 +43,7 @@
                         rowData.push(siteDetails[dataPoint])
                     }
 					tableData.push(rowData)
-					if (csvRow==3){break;}
+					//if (csvRow==3){break;}
 				}
 				table.appendRows(tableData)
 				doneCallBack();
